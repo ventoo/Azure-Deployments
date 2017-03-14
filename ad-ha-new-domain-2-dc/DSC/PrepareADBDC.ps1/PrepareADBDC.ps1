6b506b612e06bc209e461abf08d0fccbba36e9e7
Andreas Luethi <andreas.luethi@ventoo.ch>
2017-03-14 06:13:04 +0100

    Changed to single disk AD server

---
+++ ad-ha-new-domain-2-dc/DSC/PrepareADBDC.ps1/PrepareADBDC.ps1
@@ -0,0 +1,37 @@
+﻿configuration PrepareADBDC
+{
+   param
+    (
+        [Parameter(Mandatory)]
+        [String]$DNSServer,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName  xDisk, cDisk, xNetworking
+    $Interface=Get-NetAdapter|Where Name -Like "Ethernet*"|Select-Object -First 1
+    $InterfaceAlias=$($Interface.Name)
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+        WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+        
+        xDnsServerAddress DnsServerAddress
+        {
+            Address        = $DNSServer
+            InterfaceAlias = $InterfaceAlias
+            AddressFamily  = 'IPv4'
+            DependsOn="[WindowsFeature]ADDSInstall"
+        }
+   }
+}
